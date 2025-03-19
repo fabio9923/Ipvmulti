61a0673e0fbbf108a1c482f4f8e57666d5bf3805
fabio9923 <eragonvaldespaz@gmail.com>
2025-03-19 10:44:03 -0600

    first commit

---
+++ Source/Ipvmulti/IpvmultiGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IpvmultiGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AIpvmultiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AIpvmultiGameMode();
+};
+
+
+
