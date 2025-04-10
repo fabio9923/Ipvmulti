c1d3fd9f7eaef3b3639adc5fdcc139d77c1c0bff
fabio9923 <eragonvaldespaz@gmail.com>
2025-04-09 18:47:00 -0600

    Created BP_LaunchPad2 - P1 - FabioValdesPaz

--- Source/Ipvmulti/Private/Actors/LaunchPad.cpp
+++ Source/Ipvmulti/Private/Actors/LaunchPad.cpp
@@ -13,20 +13,26 @@ ALaunchPad::ALaunchPad()
 	OverlapComp = CreateDefaultSubobject<UBoxComponent>("BoxComp");
 	RootComponent = OverlapComp;
 	MeshComp->SetupAttachment(OverlapComp);
+	Launchforce = 1000;
+	LaunchAngle = 45;
 }
 
 // Called when the game starts or when spawned
 void ALaunchPad::BeginPlay()
 {
 	Super::BeginPlay();
-	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &ALaunchPad::OverLapLaunchPad);
+	OverlapComp -> OnComponentBeginOverlap.AddDynamic(this, &ALaunchPad::OverlapLaunchpad);
 }
 
-void ALaunchPad::OverLapLaunchPad(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+void ALaunchPad::OverlapLaunchpad(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
+	FRotator LaunchDirection = GetActorRotation();
+	LaunchDirection.Pitch += LaunchAngle;
+	FVector LaunchVelocity = LaunchDirection.Vector() * Launchforce;
 	ACharacter* MyCharacter = Cast<ACharacter>(OtherActor);
 	if (MyCharacter)
 	{
+		MyCharacter ->LaunchCharacter(LaunchVelocity, true, true);
 		if (GEngine)
 		{
 			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Overlap"));
--- Source/Ipvmulti/Public/Actors/LaunchPad.h
+++ Source/Ipvmulti/Public/Actors/LaunchPad.h
@@ -19,19 +19,23 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UStaticMeshComponent* MeshComp;
-	
+
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UBoxComponent* OverlapComp;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	float Launchforce;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	float LaunchAngle;
 	
 	UFUNCTION()
-	void OverLapLaunchPad(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	void OverlapLaunchpad(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	
-	
 };
